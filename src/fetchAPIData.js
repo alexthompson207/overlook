b65a29b2c3629c41220eb77cdfea700d94579d1d
Alex Thompson <ahthomps1@gmail.com>
2021-03-06 20:10:15 -0700

    Create GET requests for all customer, booking, and room data

--- src/fetchAPIData.js
+++ src/fetchAPIData.js
@@ -1,4 +1,23 @@
 export const apiRequest = {
 
+  getBookingData() {
+    return fetch('http://localhost:3001/api/v1/bookings')
+      .then(response => response.json())
+      .then(data => data.bookings)
+      .catch(error => console.log(error.message));
+  },
 
+  getCustomerData() {
+    return fetch('http://localhost:3001/api/v1/customers')
+      .then(response => response.json())
+      .then(data => data.customers)
+      .catch(error => console.log(error.message));
+  },
+
+  getRoomData() {
+    return fetch('http://localhost:3001/api/v1/rooms')
+      .then(response => response.json())
+      .then(data => data.rooms)
+      .catch(error => console.log(error.message));
+  },
 }
