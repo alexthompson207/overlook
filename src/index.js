c6906af66722fcbb59c134de74c9a29ef165ef9b
Alex Thompson <ahthomps1@gmail.com>
2021-03-06 22:06:29 -0700

    Create display functions to display guest dashboard

--- src/index.js
+++ src/index.js
@@ -34,10 +34,9 @@ function assignAPIData(customers, bookings, rooms) {
 }
 
 function createGuest() {
-  currentGuest = new Guest('customer1', bookingData);
+  currentGuest = new Guest('customer1', customerData);
   activateGuestMethods();
   displayGuestDashboard();
-
 }
 
 function activateGuestMethods() {
@@ -51,28 +50,58 @@ function displayGuestDashboard() {
   displayPastGuestBookings();
   displayGuestBookingsToday();
   displayGuestFutureBookings();
+  displayGuestName();
+  displayGuestCost();
+  displayTodaysDate();
 }
 
 function displayPastGuestBookings() {
-  const pastBookingSection = document.querySelector('.guest-bookings-past').children[2];
-  currentGuest.pastBookings.forEach(booking => {
-    const pastCard = `<li class="display-history">You stayed in room ${booking.roomNumber} on ${booking.date}</li>`;
-    pastBookingSection.insertAdjacentHTML('beforeend', pastCard);
-  })
+  const pastBookingSection = document.querySelector('.guest-bookings-past');
+  if (currentGuest.pastBookings.length === 0) {
+    pastBookingSection.insertAdjacentHTML('beforeend', `<p class="display-history">You have not stayed here before! We look forward to having you.</p>`);
+  } else {
+    currentGuest.pastBookings.forEach(booking => {
+      pastBookingSection.insertAdjacentHTML('beforeend', `<li class="display-history">You stayed in room ${booking.roomNumber} on ${booking.date}</li>`);
+    });
+  }
 }
 
 function displayGuestBookingsToday() {
-  const todayBookingSection = document.querySelector('.guest-bookings-today').children[2];
-  currentGuest.currentBookings.forEach(booking => {
-    const currentCard = `<li class="display-history">You stayed in room ${booking.roomNumber} on ${booking.date}</li>`;
-    todayBookingSection.insertAdjacentHTML('beforeend', currentCard);
-  })
+  const todayBookingSection = document.querySelector('.guest-bookings-today');
+  if (currentGuest.currentBookings.length === 0) {
+    todayBookingSection.insertAdjacentHTML('beforeend', `<p class="display-history">You don't have any stays booked today</p>`);
+  } else {
+    currentGuest.currentBookings.forEach(booking => {
+      todayBookingSection.insertAdjacentHTML('beforeend', `<li class="display-history">You are staying in Room ${booking.roomNumber} tonight</li>`);
+    });
+  }
 }
 
 function displayGuestFutureBookings() {
-  const futureBookingSection = document.querySelector('.guest-bookings-future').children[2];
-  currentGuest.currentBookings.forEach(booking => {
-    const futureCard = `<li class="display-history">You stayed in room ${booking.roomNumber} on ${booking.date}</li>`;
-    futureBookingSection.insertAdjacentHTML('beforeend', futureCard);
-  })
+  const futureBookingSection = document.querySelector('.guest-bookings-future');
+  if (currentGuest.futureBookings.length === 0) {
+    futureBookingSection.insertAdjacentHTML('beforeend', `<p class="display-history">You don't have any stays planned in the future.</p>`);
+  } else {
+    currentGuest.futureBookings.forEach(booking => {
+      futureBookingSection.insertAdjacentHTML('beforeend', `<li class="display-history">You stayed in room ${booking.roomNumber} on ${booking.date}</li>`);
+    });
+  }
+}
+
+function displayGuestName() {
+  currentGuest.getValidUserID();
+  const guestName = document.querySelector('.guest-nav-name')
+  guestName.innerText = currentGuest.getGuestName();
+}
+
+function displayGuestCost() {
+  const guestCost = document.querySelector('.guest-nav-cost');
+  guestCost.innerText = `You have spent $${currentGuest.totalAmountSpent} on your stays so far!`;
+}
+
+function displayTodaysDate() {
+  const dateToday = document.querySelector('.guest-nav-date');
+  const date = new Date().toLocaleDateString('en-US');
+  console.log(date);
+  dateToday.innerText = `Today's Date: ${date}`;
 }
