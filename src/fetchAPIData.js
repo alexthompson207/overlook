6258bfa828f12a08eea8b42980ba43270d834592
Alex Thompson <ahthomps1@gmail.com>
2021-03-06 20:34:25 -0700

    Create function to call GET request on page load

--- src/fetchAPIData.js
+++ src/fetchAPIData.js
@@ -4,20 +4,20 @@ export const apiRequest = {
     return fetch('http://localhost:3001/api/v1/bookings')
       .then(response => response.json())
       .then(data => data.bookings)
-      .catch(error => console.log(error.message));
+      .catch(error => console.log(error.message))
   },
 
   getCustomerData() {
     return fetch('http://localhost:3001/api/v1/customers')
       .then(response => response.json())
       .then(data => data.customers)
-      .catch(error => console.log(error.message));
+      .catch(error => console.log(error.message))
   },
 
   getRoomData() {
     return fetch('http://localhost:3001/api/v1/rooms')
       .then(response => response.json())
       .then(data => data.rooms)
-      .catch(error => console.log(error.message));
-  },
+      .catch(error => console.log(error.message))
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import './css/index.scss';
 import User from './User';
 import Guest from './Guest';
 import Hotel from './Hotel';
-import { fetchAPIData } from './fetchAPIData';
+import { apiRequest } from './fetchAPIData';
+
+let customerData;
+let bookingData;
+let roomData;
 
 
+window.addEventListener('load', getAllAPIData);
+
+function getAllAPIData() {
+  const customers = apiRequest.getCustomerData();
+  const bookings = apiRequest.getBookingData();
+  const rooms = apiRequest.getRoomData();
+  Promise.all([customers, bookings, rooms]).then(data => {
+    assignAPIData(data[0], data[1], data[2]);
+  })
+}
+
+function assignAPIData(customers, bookings, rooms) {
+  console.log(customers);
+  console.log(bookings);
+  console.log(rooms);
+
+  customerData = customers;
+  bookingData = bookings;
+  roomData = rooms;
+}
